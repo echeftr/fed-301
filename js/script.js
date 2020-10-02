edcb4cbc92df724367db49c21d3febcf50b52dd9
echeftr <69512919+echeftr@users.noreply.github.com>
2020-10-02 15:23:50 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,25 @@
+console.log("Hello Winc Academy");
+
+let name = "Earl";
+console.log(name);
+
+let nummer = 4 + 4;
+console.log(nummer);
+
+let nummer1 = "4" + "4";
+console.log(nummer1);
+
+let nummer2 = 4 - 2;
+console.log(nummer2);
+
+let nummer3 = 4 * 2;
+console.log(nummer3);
+
+let nummer4 = 4 / 2;
+console.log(nummer4);
+
+let nummer5 = 100 - (40 % 100);
+console.log(nummer5);
+
+let age = "40";
+console.log(age);
